e6498d2505d23e476e0b480deb63491029e3337f
daniela <danielaalmendras9@gmail.com>
2024-07-18 22:05:30 -0400

    comentario

--- me gusta (core)/scrip.js
+++
@@ -1,6 +0,0 @@
-function incrementLikes(button) {
-    const counter = button.nextElementSibling;
-    
-    let currentLikes = parseInt(counter.textContent);
-    counter.textContent = `${currentLikes + 1} like(s)`;
-}
